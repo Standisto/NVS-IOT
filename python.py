3184e63c73733bf01a86032ae0bc5788fbf09931
Vlabite <78845359+Vlabite@users.noreply.github.com>
2022-11-15 20:40:48 +0300

    Update python.py

--- python.py
+++ python.py
@@ -1,203 +1,71 @@
-
-
-import apt, apt.debfile
-import pathlib, stat, shutil, urllib.request, subprocess, getpass, time, tempfile
-import secrets, json, re
-import ipywidgets
-import socket
+# -*- coding: utf-8 -*-
+# author Dinar Ahmetshin 
+# site https://ahmetshin.com
+import sys 
 import os
-from IPython.display import clear_output
 
-class _NoteProgress(apt.progress.base.InstallProgress, apt.progress.base.AcquireProgress, apt.progress.base.OpProgress):
-  def init(self):
-    apt.progress.base.InstallProgress.init(self)
-    self._label = ipywidgets.Label()
-    display(self._label)
-    self._float_progress = ipywidgets.FloatProgress(min = 0.0, max = 1.0, layout = {'border':'1px solid #118800'})
-    display(self._float_progress)
-
-  def close(self):
-    self._float_progress.close()
-    self._label.close()
-
-  def fetch(self, item):
-    self._label.value = "fetch: " + item.shortdesc
-
-  def pulse(self, owner):
-    self._float_progress.value = self.current_items / self.total_items
-    return True
-
-  def status_change(self, pkg, percent, status):
-    self._label.value = "%s: %s" % (pkg, status)
-    self._float_progress.value = percent / 100.0
-
-  def update(self, percent=None):
-    self._float_progress.value = self.percent / 100.0
-    self._label.value = self.op + ": " + self.subop
-
-  def done(self, item=None):
-    pass
-
-class _MyApt:
-  def init(self):
-    self._progress = _NoteProgress()
-    self._cache = apt.Cache(self._progress)
-
-  def close(self):
-    self._cache.close()
-    self._cache = None
-    self._progress.close()
-    self._progress = None
-
-  def update_upgrade(self):
-    self._cache.update()
-    self._cache.open(None)
-    self._cache.upgrade()
-
-  def commit(self):
-    self._cache.commit(self._progress, self._progress)
-    self._cache.clear()
-
-  def installPkg(self, *args):
-    for name in args:
-      pkg = self._cache[name]
-      if pkg.is_installed:
-        print(f"{name} is already installed")
-      else:
-        pkg.mark_install()
-
-  def installDebPackage(self, name):
-    apt.debfile.DebPackage(name, self._cache).install()
-
-  def deleteInstalledPkg(self, *args):
-    for pkg in self._cache:
-      if pkg.is_installed:
-        for name in args:
-          if pkg.name.startswith(name):
-            #print(f"Delete {pkg.name}")
-            pkg.mark_delete()
-
-def _download(url, path):
-  try:
-    with urllib.request.urlopen(url) as response:
-      with open(path, 'wb') as outfile:
-        shutil.copyfileobj(response, outfile)
-  except:
-    print("Failed to download ", url)
-    raise
-
-  return True
-
-def _setupssh():
-  #Inpur Username
-  print("Create a username :")
-  user_name = input()
-  subprocess.run(["useradd", "-s", "/bin/bash", "-m", user_name])
-  subprocess.run(["adduser", user_name, "sudo"], check = True)
-  clear_output()
-
-  #Inpur Password
-  print("Create a password :")
-  user_password = getpass.getpass()
-  subprocess.run(["chpasswd"], input = f"{user_name}:{user_password}", universal_newlines = True)
-  clear_output()
-
-  #Set your ngrok Authtoken.
-  print("Copy & paste your tunnel authtoken from https://dashboard.ngrok.com/auth")
-  print("(You need to sign up for ngrok and login.)")
-  ngrok_token = getpass.getpass()
-  clear_output()
-
-  #Set your ngrok region.
-  print("Select your ngrok region :")
-  print("us - United States (Ohio)")
-  print("eu - Europe (Frankfurt)")
-  print("ap - Asia/Pacific (Singapore)")
-  print("au - Australia (Sydney)")
-  print("sa - South America (Sao Paulo)")
-  print("jp - Japan (Tokyo)")
-  print("in - India (Mumbai)")
-  ngrok_region = input()
-  clear_output()
-
-  #SSH Dropbear
-  my_apt = _MyApt()
-  my_apt.installPkg("dropbear")
-  my_apt.commit()
-  my_apt.close()
-
-  #Set Dropbear
-  f = open("../etc/default/dropbear", "w")
-  f.write("""# the TCP port that Dropbear listens on
-DROPBEAR_PORT=443
-
-# any additional arguments for Dropbear
-DROPBEAR_EXTRA_ARGS=
-
-# specify an optional banner file containing a message to be
-# sent to clients before they connect, such as "/etc/issue.net"
-DROPBEAR_BANNER=""
-
-# RSA hostkey file (default: /etc/dropbear/dropbear_rsa_host_key)
-#DROPBEAR_RSAKEY="/etc/dropbear/dropbear_rsa_host_key"
-
-# DSS hostkey file (default: /etc/dropbear/dropbear_dss_host_key)
-#DROPBEAR_DSSKEY="/etc/dropbear/dropbear_dss_host_key"
-
-# ECDSA hostkey file (default: /etc/dropbear/dropbear_ecdsa_host_key)
-#DROPBEAR_ECDSAKEY="/etc/dropbear/dropbear_ecdsa_host_key"
-
-# Receive window size - this is a tradeoff between memory and
-# network performance
-DROPBEAR_RECEIVE_WINDOW=65536
+def main():
+    username="userproxy"
+    try:
+        password_proxy =raw_input("type your password here: ")
+    except:
+        password_proxy =input("type your password here: ")
+    os.system("apt-get update")
+    os.system("apt-get -y install build-essential libwrap0-dev libpam0g-dev libkrb5-dev libsasl2-dev")
+    os.system("wget --no-check-certificate https://ahmetshin.com/static/dante.tgz")
+    os.system("tar -xvpzf dante.tgz")
+    os.system("apt-get -y install libwrap0 libwrap0-dev")
+    os.system("apt-get -y install gcc make")
+    os.system("mkdir /home/dante")
+    os.system("""cd dante && ./configure --prefix=/home/dante && make && make install""")
+    os.system("""
+            echo '
+            logoutput: syslog /var/log/danted.log
+            internal: eth0 port = 1080
+            external: eth0
+             
+            socksmethod: username
+            user.privileged: root
+            user.unprivileged: nobody
+             
+            client pass {
+                from: 0.0.0.0/0 to: 0.0.0.0/0
+                log: error
+            }
+             
+            socks pass {
+                from: 0.0.0.0/0 to: 0.0.0.0/0
+                command: connect
+                log: error
+                method: username
+            }' > /home/dante/danted.conf
+                """)
+    os.system("useradd --shell /usr/sbin/nologin -m %s" % username)
+    os.system('echo "%s:%s" | chpasswd' % (username,password_proxy))
+    os.system("apt-get -y install ufw")
+    os.system("ufw status")
+    os.system("ufw allow ssh")
+    os.system("ufw allow proto tcp from any to any port 1080")
+    os.system("ufw status numbered")
+    os.system("echo 'y' | ufw enable")
+    os.system("""
+echo '#!/bin/sh -e
+sleep 20
+/home/dante/sbin/sockd -f /home/dante/danted.conf -D
+exit 0
+' > /etc/rc.local
 """)
-  f.close()
+    os.system("chmod +x /etc/rc.local")
+    os.system("chmod +x /home/dante/sbin/sockd")
+    os.system("/home/dante/sbin/sockd -f /home/dante/danted.conf -D")
+    os.system("echo 'proxy install success'")
+    os.system("echo ' '")
+    os.system("echo '________________________________'")
+    os.system("echo ' '")
+    os.system("echo \"YOUR IP ADDRESS: `hostname -I | awk '{print $1}'`\"")
+    os.system("echo 'PORT: 1080'")
+    os.system("echo 'LOGIN: %s'" % username)
+    os.system("echo 'PASSWORD: %s'" % password_proxy)
 
-  #SSH Service Restart
-  subprocess.run(["service", "dropbear", "restart"])
-
-  #Root Password
-  root_password = user_password
-  subprocess.run(["chpasswd"], input = f"root:{root_password}", universal_newlines = True)
-
-  # Ngrok Tunnel
-  if not os.path.exists('ngrok.zip'):
-    _download("https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip", "ngrok.zip")
-    shutil.unpack_archive("ngrok.zip")
-    pathlib.Path("ngrok").chmod(stat.S_IXUSR)
-
-  subprocess.run(["./ngrok", "authtoken", ngrok_token])
-  ngrok_proc = subprocess.Popen(["./ngrok", "tcp", "-region", ngrok_region, "443"])
-  time.sleep(2)
-
-  with urllib.request.urlopen("http://localhost:4040/api/tunnels") as response:
-    url = json.load(response)['tunnels'][0]['public_url']
-    m = re.match("tcp://(.+):(\d+)", url)
-
-  hostname = m.group(1)
-  port = m.group(2)
-  
-  msg = ""
-  msg += "="*48 + "\n"
-  msg += "Command to connect to the ssh server:\n"
-  msg += f"ssh -p {port} {user_name}@{socket.gethostbyname(hostname)}\n"
-  msg += "="*48 + "\n"
-  msg += "SSH Login NetMod and Others:\n"
-  msg += f"Host            : {socket.gethostbyname(hostname)}\n"
-  msg += f"Port (Dropbear) : {port}\n"
-  msg += f"Username        : {user_name}\n"
-  msg += f"Password        : {user_password}\n"
-  msg += "="*48 + "\n"
-  msg += f"🔴БОЛЬШЕ ИНФЫ У НАС В КАНАЛАХ🔴 \n"
-  msg += f"Telegram ➡️ \n"
-  msg += f"https://nashiba.pro/telegram \n"
-  msg += f"YouTube ➡️ \n"
-  msg += f"https://www.youtube.com/nashiba?sub_confirmation=1 \n"
-  return msg
-
-def startColab():
-  msg = _setupssh()
-  print(msg)
-
-startColab()
-
+if __name__ == "__main__":
+    main()
